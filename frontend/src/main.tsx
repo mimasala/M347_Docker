6037f4dd00e61cd71efeef05c917d7dbcd14544f
GioeleAmbrogini <gioele.ambrogini@edu.tbz.ch>
2023-03-21 16:27:09 +0100

    feat: started reacrouting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Home from "./Home";
 import App from './App'
 import './index.css'
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@emotion/react';
 import { darkTheme } from './themes/dark';
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <CssBaseline/>
     <ThemeProvider theme={darkTheme}>
       <App />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
