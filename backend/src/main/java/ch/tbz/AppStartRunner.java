8210fdfc4b092a9796cadbc2dba5f5dc56c19bd4
mimasala <65661176+mimasala@users.noreply.github.com>
2023-04-11 13:39:59 +0200

    kjxdflkbfjlkdjgflkdsjfg

--- backend/src/main/java/ch/tbz/AppStartRunner.java
+++ backend/src/main/java/ch/tbz/AppStartRunner.java
@@ -1,19 +1,47 @@
 package ch.tbz;
 
+import ch.tbz.domain.exercise.Exercise;
+import ch.tbz.domain.exercise.ExerciseRepo;
 import ch.tbz.domain.user.UserRepository;
+import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.InputStream;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
 @Component
 @AllArgsConstructor
 public class AppStartRunner implements CommandLineRunner {
     @Autowired
-    private final UserRepository userRepository;
+    private final ExerciseRepo exerciseRepo;
 
     @Override
     public void run(String... args) throws Exception {
-
+        int offset = 0;
+        try {
+            while (true){
+                URL url = new URL("https://api.api-ninjas.com/v1/exercises?offset=" + offset);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("accept", "application/json");
+                connection.addRequestProperty("X-Api-Key", "nC7RuBqgRN9bss2W4Lom0w==4pbIHmsdENwDz8jm");
+                InputStream responseStream = connection.getInputStream();
+                Gson gson = new Gson();
+                Exercise[] exercises = gson.fromJson(new java.io.InputStreamReader(responseStream), Exercise[].class);
+                if (exercises.length == 0){
+                    break;
+                }
+//                exerciseRepo.saveAll(Arrays.asList(exercises));
+                System.out.println(Arrays.asList(exercises));
+                offset += 10;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- backend/src/main/java/ch/tbz/domain/exercise/Exercise.java
+++ backend/src/main/java/ch/tbz/domain/exercise/Exercise.java
@@ -1,14 +1,8 @@
 package ch.tbz.domain.exercise;
 
 import ch.tbz.domain.set.Set;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
@@ -20,6 +14,7 @@ import java.util.List;
 @Table(name = "_exercise")
 public class Exercise {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String type;
